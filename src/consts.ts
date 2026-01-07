bd8577d8bf17fb861a27bc3c530d57758be164b2
Ruby <93859705+MisterRuby@users.noreply.github.com>
2026-01-07 12:34:17 +0900

    feat: 레이아웃 구조 변경

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Rubit Blog';
+export const SITE_TITLE = "Rubit's Blog";
 export const SITE_DESCRIPTION = '개발과 기술에 대한 생각을 기록하는 공간입니다.';
