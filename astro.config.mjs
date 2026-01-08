1432b34ef03c9f651726cda9154b5fb92c7be520
Ruby <93859705+MisterRuby@users.noreply.github.com>
2026-01-08 09:00:44 +0900

    feat: cloudflare page url 설정

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://rubit-blog.rubit8723.workers.dev',
 	integrations: [mdx(), sitemap()],
 });
